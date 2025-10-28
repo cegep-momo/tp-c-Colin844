a1f58857eda6c74fdea3e4f76f6b80a5e40ece69
Colin Leblanc <colin.leblanc5@gmail.com>
2025-10-28 00:19:37 +0000

    [ADD] UI: Numerotation automatique des ids utilisateurs

--- user.h
+++ user.h
@@ -8,6 +8,7 @@ using namespace std;
 
 class User {
 private:
+    static int nextId; // Compteur pour générer des IDs
     string name;
     string userId;
     vector<string> borrowedBooks; // Store ISBNs of borrowed books
@@ -16,15 +17,18 @@ public:
     // Constructors
     User();
     User(const string& name, const string& userId);
+    User(const string& name); // Constructeur avec génération d'un Id
     
     // Getters
     string getName() const;
     string getUserId() const;
     vector<string> getBorrowedBooks() const;
+    static int getNextId(); // Obtenir le prochain ID disponible
     
     // Setters
     void setName(const string& name);
     void setUserId(const string& userId);
+    static void setNextId(int id); // Initialiser le compteur d'ID
     
     // Methods
     void borrowBook(const string& isbn);
